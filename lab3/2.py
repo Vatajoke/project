9df19e83acd7cefb56fc77884783685f1fcba6b4
Voloshyn Volodymyr <voloshynvo@gmail.com>
2020-04-19 16:50:06 +0300

    poster

---
+++ lab3/2.py
@@ -0,0 +1,10 @@
+from graph import *
+
+windowSize(500, 350)
+brushColor(102, 204, 255)
+rectangle(0, 0, 500, 150)
+brushColor("blue")
+rectangle(0, 150, 500, 230)
+brushColor("yellow")
+rectangle(0, 230, 500, 350)
+run()
