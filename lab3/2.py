274c1f1c83703c0b47ad396b17997b80cd7c6c9d
Voloshyn Volodymyr <voloshynvo@gmail.com>
2020-04-19 20:34:22 +0300

    sky

--- lab3/2.py
+++ lab3/2.py
@@ -6,5 +6,13 @@ rectangle(0, 0, 500, 150)
 brushColor("blue")
 rectangle(0, 150, 500, 230)
 brushColor("yellow")
+penColor("yellow")
 rectangle(0, 230, 500, 350)
+circle(450, 40, 30)
+penColor("black")
+brushColor("white")
+for i in range(3):
+    circle(100+(i*20), 50, 15)
+for i in range(4):
+    circle(90 + (i * 20), 70, 15)
 run()
