283f5c07340821a90ab009dbf0c307788782ac0c
Voloshyn Volodymyr <voloshynvo@gmail.com>
2020-04-19 15:57:32 +0300

    eyes

--- lab3/1.py
+++ lab3/1.py
@@ -5,6 +5,13 @@ brushColor("yellow")
 circle(250, 300, 100)
 brushColor("black")
 rectangle(200, 330, 300, 350)
+brushColor("red")
+circle(200, 270, 20)
+circle(300, 270, 15)
+brushColor("black")
+circle(200, 270, 8)
+circle(300, 270, 8)
+
 
 run()
 
